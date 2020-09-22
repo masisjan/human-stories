79d2e2a0186a3503fea56ae1df92fd1a5bf827d6
masisjan <mailjan@list.ru>
2020-09-22 17:25:14 +0400

    rout grup admin midelware

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -15,7 +15,7 @@ class Admin
      */
     public function handle($request, Closure $next)
     {
-        if (auth()->user()->type = 'user') {
+        if (auth()->user()->type == 'user') {
             return redirect('home');
         }
         return $next($request);
